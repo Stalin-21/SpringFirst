07ea39fd443e194ffff7195a69dffbb5227baf7e
stalin-21 <stalinguna21@gmail.com>
2025-12-27 15:04:14 +0530

    Day2 Assignment2 UC3: GET hello using path variable

--- springapi/src/main/java/com/springapi/assignment2/controller/HelloMessagingController.java
+++ springapi/src/main/java/com/springapi/assignment2/controller/HelloMessagingController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/hello-rest")
@@ -18,6 +19,12 @@ public class HelloMessagingController {
     // UC2: Query Parameter
     @GetMapping("/query")
     public String helloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    // UC3: Path Variable
+    @GetMapping("/param/{name}")
+    public String helloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }
